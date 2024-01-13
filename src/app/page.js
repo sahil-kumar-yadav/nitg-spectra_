2e768c9cd9f0f3bbdbc959f8070fb332b6b5aafa
SAHIL YADAV <121011155+yadavsk098@users.noreply.github.com>
2024-01-13 16:43:51 +0530

    Basic web structure

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>Home</div>
+    //navbar
+    //hero section
+    //Event
+    //slider
+    //footer
+  )
+}
