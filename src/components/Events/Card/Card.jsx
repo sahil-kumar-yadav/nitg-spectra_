e46234d49b30c771d5fa794251b78e9daca701f2
Kalyan Sharma <kalyansharma855@gmail.com>
2024-02-22 20:02:53 +0530

    margin added in events card

--- src/components/Events/Card/Card.jsx
+++ src/components/Events/Card/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
 
     return (
         <>
-            <div className="bg-white p-6 rounded-md shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105"
+            <div className="bg-white mx-4 py-6 rounded-md shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105"
                 data-aos="fade-up" data-aos-duration="800">
                 <h3 className="text-xl lg:text-2xl font-bold mb-2">{props.title}</h3>
                 <p className="text-gray-600">{props.date}</p>
